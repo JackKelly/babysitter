f94b40ec7e54e1c7a0e1e934a2c7353e41f8be15
Jack Kelly <jack-list@xlk.org.uk>
2013-01-22 17:22:25 +0000

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "babysitter",
+    version = "0.10",
+    packages = find_packages(),
+    install_requires = [],
+    author = "Jack Kelly",
+    author_email = "",
+    description = "Monitor files and processes and email heartbeat",
+    license = "MIT",
+    keywords = "python monitor",
+    url = "https://github.com/JackKelly/babysitter",
+    download_url = "https://github.com/JackKelly/babysitter/tarball/master"
+)
