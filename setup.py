91bc1e32f81d43a574a09c708cdf88c2a7a262b0
Jack Kelly <jack-list@xlk.org.uk>
2013-01-23 14:40:10 +0000

    Adding #egg=babysitter

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     license = "MIT",
     keywords = "python monitor",
     url = "https://github.com/JackKelly/babysitter",
-    download_url = "https://github.com/JackKelly/babysitter/tarball/master",
+    download_url = "https://github.com/JackKelly/babysitter/tarball/master#egg=babysitter",
     long_description = open('README.md').read()
 )
