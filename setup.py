867726737a60767e3abe9f5b3b288b074b674fd9
Jack Kelly <jack-list@xlk.org.uk>
2013-01-23 14:45:49 +0000

    Adding -dev

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+print find_packages()
+
 setup(
     name = "babysitter",
     version = "0.1",
@@ -11,6 +13,6 @@ setup(
     license = "MIT",
     keywords = "python monitor",
     url = "https://github.com/JackKelly/babysitter",
-    download_url = "https://github.com/JackKelly/babysitter/tarball/master#egg=babysitter",
+    download_url = "https://github.com/JackKelly/babysitter/tarball/master#egg=babysitter-dev",
     long_description = open('README.md').read()
 )
